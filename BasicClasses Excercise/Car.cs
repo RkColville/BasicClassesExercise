f56564d91fbe83a60c6c012dedacbb45b65eeab2
Robert Colville <rkcolville@gmail.com>
2021-06-01 07:39:52 -0500

    initial commit

---
+++ BasicClasses Excercise/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClasses_Excercise
+{
+    public class Car
+    {
+        public string Make;
+        public string Model;
+        public int Year;
+
+    }
+}
---
+++ BasicClasses Excercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicClasses_Excercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "Tesla";
+            car1.Model = "CyberTruck";
+            car1.Year = 2021;
+            Console.WriteLine(car1.Make);
+            Console.WriteLine(car1.Model);
+            Console.WriteLine(car1.Year);
+        }
+    }
+}
